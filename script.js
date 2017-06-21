c762368042532cb962fe131d1f0e326fb32c7f7f
Zack P <chips005@gmail.com>
2017-06-20 21:41:36 -0700

    2nd commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+function calculateGrade () {
+  let grade = document.getElementByID ("myGradeId").value;
+
+
+if (grade >= 90 && <= 100) {
+  console.log(A);
+}
+else if {
+  (grade >= 80 && < 90)
+  console.log(B);
+}
+else if {
+  (grade >= 70 && < 80);
+  console.log(C);
+}
+else if {
+  (grade >= 60 && < 70)
+  console.log(D);
+} else {
+  console.log(F);
+}
